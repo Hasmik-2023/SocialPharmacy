327563dbc09f4867d30b259f31bf830f831eae17
VladimirKuznetsov1990 <fixer2026@gmail.com>
2024-06-21 11:52:25 +0300

    modalv3

--- client/src/components/ui/Cards.jsx
+++ client/src/components/ui/Cards.jsx
@@ -12,8 +12,9 @@ export default function Cards({ card }) {
       <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
         {/* <Card.Title>{card.title}</Card.Title> */}
         <Card.Text><h4>{card.name}</h4></Card.Text>
+        <Card.Text>{`${card.price} руб.`}</Card.Text>
         <Button variant="primary">Добавить в Корзину</Button>
-        <Button onClick={() => setContent(card.description)} variant="primary">подробнее...</Button>
+        <Button onClick={() => setContent({ description: card.description, name: card.name })} variant="primary">подробнее...</Button>
       </Card.Body>
     </Card>
 
